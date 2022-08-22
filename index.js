a2362ebca45b318f50ac6c2e0dc81a1d4bf1987d
Ryan <ryanlangus@gmail.com>
2022-08-21 21:41:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+
+document.getElementById('carousel-button-next').addEventListener("click", moveToNextSlide);
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+    
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+
+
+// Tried to automate the carousel using newer js function, but couldn't get it to work
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//       const intersecting = entry.isIntersecting
+//       entry.slides = intersecting ? slidePosition ++
+//     })
+//   })
+
+//   observer.observe(document.getElementById("carousel"))
